b4b133aaa6de1721e75aadbabac0f083afd77db0
Saskue <clem.rieuneau@gmail.com>
2023-10-18 15:56:28 +0200

    Sync changes

--- interface/src/pages/Accueil.js
+++ interface/src/pages/Accueil.js
@@ -2,12 +2,12 @@ import React from 'react';
 
 const Home = () => {
     const handleClick = () => {
-        window.location.href = 'https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley'; // Remplacez par le lien de la vidéo YouTube
+        window.location.href = 'https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley';
     };
 
     return (
         <div>
-            <h1 onClick={handleClick} style={{ cursor: 'pointer' }}>feur</h1>
+            <h1 onClick={handleClick}>feur</h1>
         </div>
     );
 };
