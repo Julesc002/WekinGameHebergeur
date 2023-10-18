d5432c8eb8ad3ef2d892384746e7be5f42a6f176
LololeJB <mlolo2003@gmail.com>
2023-10-18 11:05:09 +0200

    commencement de la recherche d'une entrée

--- demo/src/main/java/com/wekinGame/Controllers/DemoApplication.java
+++ demo/src/main/java/com/wekinGame/Controllers/DemoApplication.java
@@ -3,6 +3,7 @@ import org.bson.Document;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -38,5 +39,9 @@ public class DemoApplication {
         }
 
         return String.format(res);
+    }
+    @GetMapping("/searchEntry/{data}")
+    public String searchEntry(@PathVariable String data){
+        Document searchQuery
     }
 }
