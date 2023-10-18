d119a0c84fc1d6a965138627c395b3739b77655b
Jules Cayrol <julescayrol@gmail.com>
2023-10-18 11:03:29 +0200

    Implémentation d'un getWiki basique

--- demo/src/main/java/com/wekinGame/Controllers/DemoApplication.java
+++ demo/src/main/java/com/wekinGame/Controllers/DemoApplication.java
@@ -3,6 +3,7 @@ import org.bson.Document;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,7 +19,6 @@ import com.mongodb.client.MongoDatabase;
 public class DemoApplication {
     MongoClient mongoClient = MongoClients.create("mongodb+srv://gamer:ratio@bdwekingame.decr9eq.mongodb.net/");
     MongoDatabase database = mongoClient.getDatabase("WekinGame");
-    MongoCollection<Document> collection = database.getCollection("wikis");
 
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
@@ -28,6 +28,24 @@ public class DemoApplication {
     public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
         Document searchQuery = new Document();
         searchQuery.put("_id", 1);
+        MongoCollection<Document> collection = database.getCollection("wikis");
+        FindIterable<Document> cursor = collection.find(searchQuery);
+
+        String res = "";
+        try (final MongoCursor<Document> cursorIterator = cursor.cursor()) {
+            while (cursorIterator.hasNext()) {
+                res += cursorIterator.next();
+            }
+        }
+
+        return String.format(res);
+    }
+
+    @GetMapping("/wiki/{nom}")
+    public String getWiki(@PathVariable("nom") String nom) {
+        Document searchQuery = new Document();
+        searchQuery.put("nom", nom);
+        MongoCollection<Document> collection = database.getCollection("wikis");
         FindIterable<Document> cursor = collection.find(searchQuery);
 
         String res = "";
