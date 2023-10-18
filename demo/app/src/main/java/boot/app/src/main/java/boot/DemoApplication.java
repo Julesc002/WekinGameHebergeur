8b7fe0390c299c9461747306d0d56e19822cd4ff
Alice Mailhes <alice.mailhes@etu.iut-tlse3.fr>
2023-10-18 16:39:24 +0200

    Implementation of swagger2 yaml first api generation

--- demo/app/src/main/java/boot/app/src/main/java/boot/DemoApplication.java
+++ demo/app/src/main/java/boot/app/src/main/java/boot/DemoApplication.java
@@ -1,4 +1,4 @@
-package com.example.demo;
+package app.src.main.java.boot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,11 +8,12 @@ import org.springframework.web.bind.annotation.RestController;
 @SpringBootApplication
 @RestController
 public class DemoApplication {
-    public static void main(String[] args) {
-      SpringApplication.run(DemoApplication.class, args);
-    }
-    @GetMapping("/")
-    public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
-      return String.format("Hello %s!", name);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+	@GetMapping("/")
+	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+		return String.format("Hello %s!", name);
+	}
 }
