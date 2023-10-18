d2f69f74a9cb3225b0e6039c6859ad2ecdad8fc6
Saskue <clem.rieuneau@gmail.com>
2023-10-18 11:16:31 +0200

    Fonction de recherche d'un wiki qui commence par une valeur entrée en paramètre

---
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,70 @@
+package com.example.demo;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.bson.Document;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.mongodb.client.FindIterable;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoCursor;
+import com.mongodb.client.MongoDatabase;
+
+@SpringBootApplication
+@RestController
+public class DemoApplication {
+  MongoClient mongoClient = MongoClients.create("mongodb+srv://gamer:ratio@bdwekingame.decr9eq.mongodb.net/");
+  MongoDatabase database = mongoClient.getDatabase("WekinGame");
+  MongoCollection<Document> collection = database.getCollection("wikis");
+
+  public static void main(String[] args) {
+    SpringApplication.run(DemoApplication.class, args);
+  }
+
+  @GetMapping("/")
+  public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+    Document searchQuery = new Document();
+    searchQuery.put("_id", 1);
+    FindIterable<Document> cursor = collection.find(searchQuery);
+
+    String res = "";
+    try (final MongoCursor<Document> cursorIterator = cursor.cursor()) {
+      while (cursorIterator.hasNext()) {
+        res += cursorIterator.next();
+      }
+    }
+
+    return String.format(res);
+  }
+
+  @GetMapping("/search/wiki")
+  public String getWikisByPrefix(@RequestParam(value = "game") String game) {
+    List<Document> results = searchWikisByPrefix(game);
+    return results.toString();
+  }
+
+  private List<Document> searchWikisByPrefix(String prefix) {
+    List<Document> results = new ArrayList<>();
+
+    Document searchQuery = new Document();
+    searchQuery.put("nom", new Document("$regex", "^" + prefix));
+
+    FindIterable<Document> cursor = collection.find(searchQuery);
+
+    try (final MongoCursor<Document> cursorIterator = cursor.cursor()) {
+      while (cursorIterator.hasNext()) {
+        results.add(cursorIterator.next());
+      }
+    }
+
+    return results;
+  }
+
+}
