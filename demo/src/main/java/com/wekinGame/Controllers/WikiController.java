2c2f583b9d6fa487ed13891ad2fdacebaa8ed511
Saskue <clem.rieuneau@gmail.com>
2023-10-19 08:50:47 +0200

    getWikisByPrefix retourne un tableau vide lorsqu'on écrit 'game='

--- demo/src/main/java/com/wekinGame/Controllers/WikiController.java
+++ demo/src/main/java/com/wekinGame/Controllers/WikiController.java
@@ -7,6 +7,7 @@ import org.bson.Document;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.mongodb.client.FindIterable;
 import com.mongodb.client.MongoClient;
@@ -15,6 +16,7 @@ import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoCursor;
 import com.mongodb.client.MongoDatabase;
 
+@RestController
 public class WikiController {
 
     MongoClient mongoClient = MongoClients.create("mongodb+srv://gamer:ratio@bdwekingame.decr9eq.mongodb.net/");
@@ -48,19 +50,23 @@ public class WikiController {
 
     private List<Document> searchWikisByPrefix(String prefix) {
         List<Document> results = new ArrayList<>();
+        if (prefix.length() == 0) {
+            return results;
+        } else {
 
-        Document searchQuery = new Document();
-        searchQuery.put("nom", new Document("$regex", "^" + prefix).append("$options", "i"));
+            Document searchQuery = new Document();
+            searchQuery.put("nom", new Document("$regex", "^" + prefix).append("$options", "i"));
 
-        MongoCollection<Document> collection = database.getCollection("wikis");
-        FindIterable<Document> cursor = collection.find(searchQuery);
+            MongoCollection<Document> collection = database.getCollection("wikis");
+            FindIterable<Document> cursor = collection.find(searchQuery);
 
-        try (final MongoCursor<Document> cursorIterator = cursor.cursor()) {
-            while (cursorIterator.hasNext()) {
-                results.add(cursorIterator.next());
+            try (final MongoCursor<Document> cursorIterator = cursor.cursor()) {
+                while (cursorIterator.hasNext()) {
+                    results.add(cursorIterator.next());
+                }
             }
-        }
 
-        return results;
+            return results;
+        }
     }
 }
