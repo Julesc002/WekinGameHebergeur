78fb6da1767378b044e170b392a2fa7ac6d931be
LololeJB <mlolo2003@gmail.com>
2023-10-19 11:03:52 +0200

    Modification de la sortie de searchEntry(), retournant : nom entrée, id entrée, catégories, nom wiki

--- demo/src/main/java/com/wekinGame/Controllers/EntryController.java
+++ demo/src/main/java/com/wekinGame/Controllers/EntryController.java
@@ -1,20 +1,25 @@
 package com.wekinGame.Controllers;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import org.bson.Document;
+import org.bson.conversions.Bson;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.mongodb.client.AggregateIterable;
 import com.mongodb.client.FindIterable;
 import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoCursor;
 import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.model.Aggregates;
+import com.mongodb.client.model.Projections;
 
 @RestController
 public class EntryController {
@@ -23,20 +28,20 @@ public class EntryController {
     MongoDatabase database = mongoClient.getDatabase("WekinGame");
     
     @GetMapping("/entry/{idEntry}")
-    public String getEntry(@PathVariable("idEntry") String idEntry) {
+    public List<Document> getEntry(@PathVariable("idEntry") String idEntry) {
         Document searchQuery = new Document();
         searchQuery.put("_id", Integer.parseInt(idEntry));
         MongoCollection<Document> collectionEntrees = database.getCollection("entrees");
         FindIterable<Document> cursor = collectionEntrees.find(searchQuery);
 
-        String res = "";
+        List<Document> results = new ArrayList<>();
         try (final MongoCursor<Document> cursorIterator = cursor.cursor()) {
             while (cursorIterator.hasNext()) {
-                res += cursorIterator.next();
+                results.add(cursorIterator.next());
             }
         }
 
-        return String.format(res);
+        return results;
     }
 
     @GetMapping("/searchEntry")
@@ -44,8 +49,17 @@ public class EntryController {
         List<Document> results = new ArrayList<Document>();
         Document searchQuery = new Document();
         searchQuery.put("nom", new Document("$regex", data).append("$options", "i"));
+        List<Bson> pipeline = Arrays.asList(
+            Aggregates.match(searchQuery),  // Filtrer les documents dans la collection actuelle
+            Aggregates.lookup("wikis", "id_wiki", "_id", "wiki"),  // Fusionner avec une autre collection
+            Aggregates.unwind("$wiki"),  // "Déplier" le résultat de la fusion
+            Aggregates.project(Projections.fields(
+                Projections.include("_id", "nom", "categories", "wiki.nom")  // Sélectionner les champs nécessaires
+            ))
+        );
+
         MongoCollection<Document> collection = database.getCollection("entrees");
-        FindIterable<Document> cursor = collection.find(searchQuery);
+        AggregateIterable<Document> cursor = collection.aggregate(pipeline);
 
         try (final MongoCursor<Document> cursorIterator = cursor.cursor()) {
             while (cursorIterator.hasNext()) {
