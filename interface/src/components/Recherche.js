1610178eec3deeb242bd692cec434e6aeb43c9c6
Jules Cayrol <julescayrol@gmail.com>
2023-10-19 09:31:00 +0200

    modification icon du site

--- interface/src/components/Recherche.js
+++ interface/src/components/Recherche.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import axios from 'axios';
 import { API_WIKI_URL } from '../config';
 import _ from 'lodash';
