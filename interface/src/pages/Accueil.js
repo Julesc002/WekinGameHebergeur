4ac20b11bc5a6120f5649952a220046868bd8106
LololeJB <mlolo2003@gmail.com>
2023-10-20 08:30:28 +0200

    remplacement du "Feur" par "WekinGame" dans Accueil.js

--- interface/src/pages/Accueil.js
+++ interface/src/pages/Accueil.js
@@ -3,12 +3,12 @@ import Recherche from '../components/Recherche';
 
 const Home = () => {
     const handleClick = () => {
-        window.location.href = 'https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley';
+        window.location.href = 'https://www.youtube.com/watch?v=dQw4w9WgXcQ';
     };
 
     return (
         <div>
-            <h1 onClick={handleClick}>feur</h1>
+            <h1 onClick={handleClick}>WekinGame V0</h1>
             <h4>Rechercher dans tous les wikis :</h4>
             <Recherche />
         </div>
