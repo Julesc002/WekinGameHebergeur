f30e891edd8cdc3b7156228bc2bc12f1021b7bcd
Jules Cayrol <julescayrol@gmail.com>
2023-10-23 15:14:51 +0200

    ajout page avec un message d'erreur quand l'url n'existe pas

--- interface/src/App.js
+++ interface/src/App.js
@@ -6,12 +6,14 @@ import Layout from './components/Layout';
 import DetailEntree from './components/DetailEntree';
 import DetailCategorie from './components/DetailCategorie';
 import WikiContent from './components/WikiContent';
+import MessageErreur from './components/MessageErreur';
 
 const App = () => {
 
   return (
     <Layout>
       <Routes>
+        <Route path='*' element={<MessageErreur />} />
         <Route path='/' element={<Accueil />} />
         <Route path="/entree/:id" element={<DetailEntree />} />
         <Route path="/categorie/:id/:nom" element={<DetailCategorie />} />
---
+++ interface/src/components/MessageErreur.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const MessageErreur = ({ children }) => {
+    return (
+        <div>
+            <h1>Mince cette page n'existe pas</h1>
+            <h6>fin c'est sad la commu...</h6>
+        </div>
+    );
+};
+
+export default MessageErreur;
