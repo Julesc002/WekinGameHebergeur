ba569ea5a7cec347abb8dbae75f7334d817701ad
Jules Cayrol <julescayrol@gmail.com>
2023-10-23 14:54:35 +0200

    Fin de la page wiki content

--- interface/src/components/WikiContent.js
+++ interface/src/components/WikiContent.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { API_URL } from '../config';
 import { useEffect, useState } from "react";
-import { useParams, useNavigate } from "react-router-dom";
+import { useParams, useNavigate, Link, NavLink } from "react-router-dom";
 
 function WikiContent() {
     const { id } = useParams();
@@ -26,20 +26,28 @@ function WikiContent() {
         <div>
             <h1>Wiki {wiki ? wiki.nom : ""}</h1>
             <p>{wiki ? wiki.description : ""}</p>
+            <NavLink to="/createCategorie">
+                <button style={{ cursor: 'pointer' }} onClick={handleRetourClick}>Ajouter une catérogie</button>
+            </NavLink>
+            <NavLink to="/createEntry">
+                <button style={{ cursor: 'pointer' }} onClick={handleRetourClick}>Ajouter une entrée</button>
+            </NavLink>
             {wiki && wiki.categories.map(function (categorie) {
                 return (
                     <div>
-                        <h3>{categorie.nom} :</h3>
+                        <Link to={`/categorie/${wiki._id}/${categorie.nom}`}>
+                            <h3 style={{ cursor: 'pointer' }}>{categorie.nom} :</h3>
+                        </Link>
                         {categorie && categorie.entrees.map(function (entree) {
                             return (
-                                <p>{entree.nom}</p>
+                                <Link to={`/entree/${entree._id}`}>
+                                    <p style={{ cursor: 'pointer' }}>{entree.nom}</p>
+                                </Link>
                             )
                         })}
                     </div>
                 )
             })}
-
-
             <button style={{ cursor: 'pointer' }} onClick={handleRetourClick}>Retour</button>
         </div>
     );
