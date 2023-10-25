1013207fbfd7ad06f463f9747a446fb11e6b5e9e
LololeJB <mlolo2003@gmail.com>
2023-10-25 16:12:38 +0200

    Création du compte, suppression de compte et connexion backend ok

--- demo/src/main/java/com/wekinGame/Controllers/Test.java
+++ demo/src/main/java/com/wekinGame/Controllers/Test.java
@@ -4,11 +4,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.wekinGame.ressources.Hasher;
+import com.wekinGame.ressources.idGenerator;
 
 @RestController
 public class Test {
     @GetMapping("/")
     public String hash(){
-        return (Hasher.hashPassword("PulpFiction"));
+        return ""+idGenerator.newUserId();
+        //return (Hasher.hashPassword("PulpFiction"));
     }
 }
--- interface/src/components/ConnexionCompte.js
+++ interface/src/components/ConnexionCompte.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import axios from 'axios';
 import { API_URL } from '../config';
 
-function CreationDeCompte(){
+function ConnexionAuCompte(){
     const[name, setName]=useState('');
     const[email, setEmail]=useState('');
     const[password, setPassword]=useState('');
