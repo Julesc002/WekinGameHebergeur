85d2f827f2f9a16c93b544f12b9fd5f86cf136e3
Alice Mailhes <alice.mailhes@etu.iut-tlse3.fr>
2023-10-25 13:23:37 +0200

    Addition of Logo and index redirect

--- interface/src/components/Header.js
+++ interface/src/components/Header.js
@@ -1,16 +1,26 @@
 import React from 'react';
+import { useNavigate } from "react-router-dom";
 import Recherche from '../components/Recherche';
 import '../styles/components/_header.css';
 
 function Header() {
-    const handleClick = () => {
+
+    const nav = useNavigate();
+    
+    const handleClickTitle = () => {
         window.location.href = 'https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley';
+    };
+    const handleClickLogo = () => {
+        nav('/');
     };
 
     return (
     <div className="Header">
       <div>
-        <h1 style={{ cursor: 'pointer' }} onClick={handleClick}>WekinGames</h1>
+        <img style={{ cursor: 'pointer' }} id="logo" src = "/wekin-games-logo.svg" alt="WekinGames logo" onClick={handleClickLogo}/>
+      </div>
+      <div>
+        <h1 style={{ cursor: 'pointer' }} onClick={handleClickTitle}>WekinGames</h1>
         <h2>Recherche parmis tout les wikis</h2>
       </div>
       <div class="bottom-right"> <Recherche /> </div>
