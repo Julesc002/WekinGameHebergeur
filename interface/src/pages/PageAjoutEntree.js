eb00f0a9908fd3bb1f05b704ffcce51981d46381
Alice Mailhes <alice.mailhes@etu.iut-tlse3.fr>
2023-10-27 11:12:23 +0200

    Amélioration style front wiki et création entrée

--- interface/src/pages/PageAjoutEntree.js
+++ interface/src/pages/PageAjoutEntree.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import Header from '../components/Header';
 import AjoutEntree from '../components/AjoutEntree';
+import Header from '../components/Header';
 
 const PageAjoutEntree = () => {
     return (
