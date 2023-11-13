ef3667d1ac9c4b931fbc9fb988d0e79abf11537c
Jules Cayrol <julescayrol@gmail.com>
2023-11-13 15:09:10 +0100

    config fichier test pour maven

--- demo/src/test/java/com/example/demo/DemoApplicationTests.java
+++ demo/src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,9 +1,14 @@
 package com.example.demo;
 
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+import com.wekinGame.RunApplication;
+import com.wekinGame.Controllers.DemoApplication;
+
+@SpringBootTest(classes = RunApplication.class)
 class DemoApplicationTests {
 
 	@Test
