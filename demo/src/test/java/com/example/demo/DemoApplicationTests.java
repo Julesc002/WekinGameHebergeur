5b4defb16273858f562c1a3ae2491573bc1d7678
LololeJB <mlolo2003@gmail.com>
2023-11-14 08:28:39 +0100

    ajout de test volontairement foireux

--- demo/src/test/java/com/example/demo/DemoApplicationTests.java
+++ demo/src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,18 +1,21 @@
 package com.example.demo;
 
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import com.wekinGame.RunApplication;
-import com.wekinGame.Controllers.DemoApplication;
 
 @SpringBootTest(classes = RunApplication.class)
 class DemoApplicationTests {
 
 	@Test
 	void contextLoads() {
+	}
+	@Test
+	void testfailer(){
+		assertTrue(1==-1);
 	}
 
  }
